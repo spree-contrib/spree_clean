a448fa08ef12f8e78f62e6911d6a595990582673
Ikenna Okpala <ikennaokpala@gmail.com>
2012-02-06 11:43:24 +0000

    Add checks/unchecks all children checkboxes within.
        * Add javascript to the spree_clean.js file for checking and unchecking all children checkboxes
        * Modify parent check_box_tag fields class attribute to .parentcheckBox

--- app/assets/javascripts/admin/spree_clean.js
+++ app/assets/javascripts/admin/spree_clean.js
@@ -1,1 +1,7 @@
 //= require admin/spree_core
+
+$(document).ready(function() {
+ 	$('.parentCheckBox').click(function () {
+		$(this).parents('li:eq(0)').find(':checkbox').attr('checked', this.checked);
+	});
+});
