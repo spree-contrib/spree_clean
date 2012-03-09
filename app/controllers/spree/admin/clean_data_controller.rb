c15587f6d8b94d7b311de27a5941714a7562c98b
John Dyer <lbrapid@gmail.com>
2012-03-09 11:02:05 -0500

    Basic destroy_data controller action in place

--- app/controllers/spree/admin/clean_data_controller.rb
+++ app/controllers/spree/admin/clean_data_controller.rb
@@ -2,7 +2,11 @@ module Spree
   module Admin
     class CleanDataController < BaseController
       def destroy_data
-
+        params[:resources].each_key do |resource|
+          "Spree::#{resource.classify}".constantize.delete_all
+          flash[:notice] = "Selected data has been successfully deleted"
+          redirect_to admin_clean_data_path
+        end
       end
     end
   end
