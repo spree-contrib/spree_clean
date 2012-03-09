ee533489d88771e173b20975966f0df03afb110f
John Dyer <lbrapid@gmail.com>
2012-03-09 13:48:26 -0500

    Deleting sample data functionality in place

--- app/controllers/spree/admin/clean_data_controller.rb
+++ app/controllers/spree/admin/clean_data_controller.rb
@@ -3,10 +3,18 @@ module Spree
     class CleanDataController < BaseController
       def destroy_data
         params[:resources].each_key do |resource|
+          case resource
+          when "user"
+            Spree::User.where('id NOT IN (SELECT user_id FROM spree_roles_users)').delete_all
+          when "address"
+            Spree::Address.delete_all
+          else
           "Spree::#{resource.classify}".constantize.delete_all
-          flash[:notice] = "Selected data has been successfully deleted"
-          redirect_to admin_clean_data_path
+          end
         end
+
+        flash[:notice] = "Selected data has been successfully deleted"
+        redirect_to admin_clean_data_path
       end
     end
   end
