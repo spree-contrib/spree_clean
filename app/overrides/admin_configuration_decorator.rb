f49fb50e88b731a1cb1f7017c52ace594deb2240
John Dyer <lbrapid@gmail.com>
2012-11-16 10:50:52 -0500

    Updated to support Spree 1.3

--- app/overrides/admin_configuration_decorator.rb
+++ app/overrides/admin_configuration_decorator.rb
@@ -1,5 +1,5 @@
-Deface::Override.new(:virtual_path => "spree/admin/configurations/index",
+Deface::Override.new(:virtual_path => "spree/admin/shared/_configuration_menu",
                      :name => "add_clean_data_to_configurations_menu",
-                     :insert_after => "[data-hook='admin_configurations_menu']",
+                     :insert_bottom => "[data-hook='admin_configurations_sidebar_menu']",
                      :partial => "spree/admin/shared/configurations_menu",
                      :disabled => false)
